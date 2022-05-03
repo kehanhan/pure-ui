211556b8b35302810af5bcd908ba1bc1484f122b
kehanhan <ke_hanhan@outlook.com>
2022-05-03 22:38:41 +0800

    1.引入sass; 2.创建Home和Doc组件; 3.添加Home和Doc路由;

---
+++ src/views/Doc.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <nav class="topnav">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </nav>
+        <div class="content">
+            <aside>
+                边栏
+            </aside>
+            <main>内容</main>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <nav class="topnav">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </nav>
+        <div class="banner">
+            <h1>Pure UI</h1>
+            <h2>一个简洁、轻量的Vue3 UI框架</h2>
+            <p class="actions">
+                <a href="https://github.com/kehanhan/pure-ui">Github</a>
+                <a>Get Started</a>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+</style>
