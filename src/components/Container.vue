63e143c04b48cc97603416941fc53d9559f2b843
kehanhan <ke_hanhan@outlook.com>
2022-05-06 17:34:08 +0800

    创建Container并注册为全局组件

---
+++ src/components/Container.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#container {
+  padding: 3em;
+  height: 100%;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './global.scss';
+import { createApp, VueElement } from "vue";
+import App from "./App.vue";
+import "./global.scss";
 import { router } from "./router";
+import Container from "./components/Container.vue";
 
 const app = createApp(App);
 app.use(router);
-app.mount('#app');
-
+app.component("Container", Container);
+app.mount("#app");
