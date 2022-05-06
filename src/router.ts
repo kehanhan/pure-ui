b901a26ad3e7a2d911a2ba59ed737effa8898b76
kehanhan <ke_hanhan@outlook.com>
2022-05-06 13:35:35 +0800

    重命名四个组件

--- src/router.ts
+++ src/router.ts
@@ -1,59 +1,59 @@
 import { createWebHashHistory, createRouter } from "vue-router";
-import Home from './views/Home.vue';
-import Doc from './views/Doc.vue';
-import Introduction from './components/Introduction.vue'
-import Installation from './components/Installation.vue'
-import GetStarted from './components/GetStarted.vue'
-import Switch from './components/Switch.vue'
-import Button from './components/Button.vue'
-import Dialog from './components/Dialog.vue'
-import Tab from './components/Tab.vue'
+import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue";
+import Introduction from "./components/Introduction.vue";
+import Installation from "./components/Installation.vue";
+import GetStarted from "./components/GetStarted.vue";
+import SwitchDemo from "./components/Demos/SwitchDemo.vue";
+import ButtonDemo from "./components/Demos/ButtonDemo.vue";
+import DialogDemo from "./components/Demos/DialogDemo.vue";
+import TabDemo from "./components/Demos/TabDemo.vue";
 
 const history = createWebHashHistory();
-export const router = createRouter(
+export const router = createRouter({
+  history,
+  routes: [
     {
-        history,
-        routes:[
-            {
-                path:'/',
-                component: Home
-            },
-            {
-                path: '/doc',
-                component: Doc,
-                children:[
-                    {
-                        path: '',
-                        component: Introduction
-                    },
-                    {
-                        path: 'introduction',
-                        component: Introduction
-                    },
-                    {
-                        path: 'installation',
-                        component: Installation
-                    },
-                    {
-                        path: 'get-started',
-                        component: GetStarted
-                    },
-                    {
-                        path: 'switch',
-                        component: Switch
-                    },
-                    {
-                        path: 'button',
-                        component: Button
-                    },{
-                        path: 'dialog',
-                        component: Dialog
-                    },{
-                        path: 'tab',
-                        component: Tab
-                    },
-                ]
-            }
-        ]
-    }
-);
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/doc",
+      component: Doc,
+      children: [
+        {
+          path: "",
+          component: Introduction,
+        },
+        {
+          path: "introduction",
+          component: Introduction,
+        },
+        {
+          path: "installation",
+          component: Installation,
+        },
+        {
+          path: "get-started",
+          component: GetStarted,
+        },
+        {
+          path: "switch",
+          component: SwitchDemo,
+        },
+        {
+          path: "button",
+          component: ButtonDemo,
+        },
+        {
+          path: "dialog",
+          component: DialogDemo,
+        },
+        {
+          path: "tab",
+          component: TabDemo,
+        },
+      ],
+    },
+  ],
+});
