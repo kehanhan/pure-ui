671d7ab3b81e2601180683cbb076f2b5d380a9de
kehanhan <ke_hanhan@outlook.com>
2022-05-09 13:47:37 +0800

    Button样式更新

--- src/components/libs/Button.vue
+++ src/components/libs/Button.vue
@@ -61,9 +61,7 @@ $green: #40b57f;
   background-color: white;
   color: #333;
   border: 1px solid $border-color;
-  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14),
-    0 1px 5px 0 rgba(0, 0, 0, 0.12);
-
+  border-radius: 4px;
   &[disabled] {
     cursor: not-allowed;
     background: #d7d7d7 !important;
