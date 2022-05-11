44e9719891da2cb9dba4ac386b085b5286225811
kehanhan <ke_hanhan@outlook.com>
2022-05-11 11:31:07 +0800

    创建Tab组件

--- src/components/demos/TabDemo.vue
+++ src/components/demos/TabDemo.vue
@@ -1,5 +1,18 @@
 <template>
-  <Container> Tab </Container>
+  <Container>
+    <h1>示例1</h1>
+    <Tabs>
+      <Tab title="Tab1">内容1</Tab>
+      <Tab title="Tab2">内容2</Tab>
+    </Tabs>
+  </Container>
 </template>
 
-<style lang="scss" scoped></style>
+<script lang="ts">
+import Tabs from "../libs/Tabs.vue";
+import Tab from "../libs/Tab.vue";
+import Container from "../Container.vue";
+export default {
+  components: { Tabs, Tab, Container },
+};
+</script>
---
+++ src/components/libs/Tabs.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>Tabs组件</div>
+  <div>
+    <component v-for="tab in defaults" :is="tab"></component>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Tab from "./Tab.vue";
+export default defineComponent({
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs 子标签必须是Tab");
+      }
+    });
+    return { defaults };
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
