a87059d8e86d9e6b894b2ff2dff438e3d2ba6c6e
kehanhan <ke_hanhan@outlook.com>
2022-05-12 21:13:49 +0800

    引入iconfont图标

---
+++ src/components/Icon.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg class="icon" aria-hidden="true">
+    <use :xlink:href="`#icon-${name}`"></use>
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+  font-size: inherit;
+}
+</style>
