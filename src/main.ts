afd65082a7467df14ce5afc50ce17816cb993d09
kehanhan <ke_hanhan@outlook.com>
2022-05-12 21:14:15 +0800

    注册Icon为全局组件

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import "./components/libs/pure.scss";
 import App from "./App.vue";
 import "./global.scss";
 import { router } from "./router";
+import "//at.alicdn.com/t/font_3397136_956a78uliy.js";
+import Icon from "./components/Icon.vue";
 import Container from "./components/Container.vue";
 
 const app = createApp(App);
 app.use(router);
 app.component("Container", Container);
+app.component("Icon", Icon);
 app.mount("#app");
