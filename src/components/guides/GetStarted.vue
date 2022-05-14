a2092e02aa960ba22a6bb30570bc9beaa7a2b88b
kehanhan <ke_hanhan@outlook.com>
2022-05-14 21:13:00 +0800

    更新 “开始使用” 文档页

--- src/components/guides/GetStarted.vue
+++ src/components/guides/GetStarted.vue
@@ -1,5 +1,22 @@
 <template>
-  <Container> Get Started </Container>
+  <Container>
+    <h1>开始使用</h1>
+    <p>在入口文件中引入CSS</p>
+    <p class="notice">
+      由于使用Vite打包时踩了一些坑，目前仅Switch组件可以正常工作
+    </p>
+    <pre><code>import "pure-ui-vue3/dist/libs/pure.css";</code></pre>
+    <p>按需引入组件</p>
+    <pre><code>import { Button, Dialog, Switch, Tab, Tabs } from "pure-ui-vue3";
+
+export default {
+  components: { Button, Dialog, Switch, Tab, Tabs }
+}</code></pre>
+  </Container>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.notice {
+  color: red;
+}
+</style>
