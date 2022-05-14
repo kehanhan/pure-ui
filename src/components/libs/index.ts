37585f3c2823e5e426dcdc2f5753c283aad0d7ac
kehanhan <ke_hanhan@outlook.com>
2022-05-14 20:58:05 +0800

    导出库文件

---
+++ src/components/libs/index.ts
@@ -0,0 +1,6 @@
+import Switch from "./Switch.vue";
+import Button from "./Button.vue";
+import Tabs from "./Tabs.vue";
+import Tab from "./Tab.vue";
+import Dialog from "./Dialog.vue";
+export { Switch, Button, Tabs, Tab, Dialog };
